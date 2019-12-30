5e017eb8284eab472fda158aeba436f7d320ab3d
hfmartinez <hmartinez@agrum.co>
2019-12-30 11:21:47 -0500

    Fix Folder Order

--- Cargador/components/components/drv/Functions.h
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-char *substring(char *string, int position, int length);
--- Cargador/components/components/lv_examples/lv_cargador/cargador/cargador.mk
+++
@@ -1,7 +0,0 @@
-CSRCS += demo.c
-CSRCS += img_bubble_pattern.c
-
-DEPPATH += --dep-path $(LVGL_DIR)/lv_examples/lv_cargador/cargador
-VPATH += :$(LVGL_DIR)/lv_examples/lv_cargador/cargador
-
-CFLAGS += "-I$(LVGL_DIR)/lv_examples/lv_cargador/cargador"
